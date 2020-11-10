85bca34011c3c28d76cc342afb3e57bc17894b62
Ivan Khamlenko <tandemloved@gmail.com>
2020-11-10 13:43:55 +0200

    Update views.py

--- drf/views.py
+++ drf/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
+from rest_framework import status
 from django.contrib.auth.models import User
-
+from django.db import transaction, models
 from .serializers import TransferSerializer
 from .models import Users
 
@@ -11,42 +12,37 @@ class TransferViewSet(viewsets.ViewSet):
     serializer_class = TransferSerializer
 
     def create(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = self.get_serializer(data=request.data)  # context
         serializer.is_valid(True)
+        data = serializer.data
+        amount = data['amount']
+        with transaction.atomic():
+            user_from = User.objects.prefetch_related('users_set').filter(id=data['user_from']).first()
 
-        amount = float(request.POST['amount'])
-        sum_part = 0
+            # ищем сумму на счёте пользователя
+            us = user_from.users_set.all().first()
 
-        user_from = User.objects.get(id=request.data['user_from'])
+            if us:
+                acc_sum = us.account
 
-        # ищем сумму на счёте пользователя
-        us = user_from.users_set.all()
+                inn_to = Users.objects.filter(inn=data['inn_to'])
 
-        if us:
-            acc_sum = us[0].account
+                if inn_to.exists() and acc_sum >= amount:
+                    users_count = inn_to.count()
+                    sum_part = round(amount / users_count, 2)
 
-            inn_to = Users.objects.filter(inn=request.data['inn_to'])
-            users_count = 0
+                    # со счёта донора списать всю сумму
 
-            if inn_to and acc_sum >= amount:
-                users_count = len(inn_to)
-                sum_part = round(amount / users_count, 2)
+                    result_sum = float(acc_sum) - sum_part * users_count
+                    us.account = result_sum
+                    us.save()
 
-                # со счёта донора списать всю сумму
-                res_user = user_from.users_set.get()
-                result_sum = float(res_user.account) - sum_part * users_count
-                res_user.account = result_sum
-                res_user.save()
+                    # на счёт каждого записать по части
+                    inn_to.update(account=models.F('account') + sum_part)
 
-                # на счёт каждого записать по части
-                for i in inn_to:
-                    result_sum = float(i.account) + sum_part
-                    i.account = result_sum
-                    i.save()
-
-                return Response(serializer.data)
+                    return Response(serializer.data)
+                else:
+                    return Response('перевод не выполнен', status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response('перевод не выполнен')
-        else:
-            acc_sum = 0
-            return Response('На счёте недостаточно средств')
+
+                return Response('На счёте недостаточно средств', status=status.HTTP_400_BAD_REQUEST)
